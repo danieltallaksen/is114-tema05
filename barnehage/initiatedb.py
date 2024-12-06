075deb0fecbce3ebfbb52e0d3fb549dad9436ab7
Daniel Tallaksen <danieltallaksen@gmail.com>
2024-12-06 13:44:54 +0100

    endra bh

--- barnehage/initiatedb.py
+++ barnehage/initiatedb.py
@@ -3,15 +3,18 @@ import pandas as pd
 from kgmodel import Barnehage
 
 def initiate_db(db_name):
-    kg1 = Barnehage(1,"Sunshine Preschool",50,15)
-    kg2 = Barnehage(2,"Happy Days Nursery",25,2)
-    kg3 = Barnehage(3,"123 Learning Center",35,4)
-    kg4 = Barnehage(4,"ABC Kindergarten",12,0)
-    kg5 = Barnehage(5,"Tiny Tots Academy",15,5)
-    kg6 = Barnehage(6,"Giggles and Grins Childcare",10,0)
-    kg7 = Barnehage(7,"Playful Pals Daycare",40,6)
+    kg1 = Barnehage(1,"Falkåsen Barnehage",25,12)
+    kg2 = Barnehage(2,"Grasmyr naturbarnehage",60,7)
+    kg3 = Barnehage(3,"Nustad Barnehage",30,5)
+    kg4 = Barnehage(4,"Rønholdt Barnehage",18,2)
+    kg5 = Barnehage(5,"Sunby Barnehage",18,6)
+    kg6 = Barnehage(6,"Uksodden Barnehage",15,4)
+    kg7 = Barnehage(7,"Stokkevannet Barnehage",45,8)
+    kg8 = Barnehage(8, "Grashoppa Barnehage", 35,4)
+    kg9 = Barnehage(9, "Solstua Barnehage", 24, 8)
+    kg10 = Barnehage(10, "Tiriltoppen Barnehage", 50, 9)
     
-    barnehage_liste = [kg1, kg2, kg3, kg4, kg5, kg6, kg7]
+    barnehage_liste = [kg1, kg2, kg3, kg4, kg5, kg6, kg7, kg8, kg9, kg10]
     
     
     kolonner_forelder =  ['foresatt_id',
@@ -24,6 +27,7 @@ def initiate_db(db_name):
                           'barnehage_antall_plasser',
                           'barnehage_ledige_plasser']
     kolonner_barn = ['barn_id',
+                     'barn_navn',
                      'barn_pnr']
     kolonner_soknad = ['sok_id',
                        'foresatt_1',
@@ -50,12 +54,9 @@ def initiate_db(db_name):
         barn.to_excel(writer, sheet_name='barn')
         soknad.to_excel(writer, sheet_name='soknad')
     
-    """
-    b1 = Barn(1, "09012356472")
-    f1 = Foresatt(1, "Ole Nordmann", "Bekkeveien 100", "98434344", "09079089332")
-    f2 = Foresatt(2, "Solveig Imsdal", "Bekkeveien 100", "98434312", "09079233221")
-    """
 
-initiate_db("kgdata.xlsx")
-
-
+"""
+b1 = Barn(1, "09012356472")
+    f1 = Foresatt(1, "Olav Hansen", "Øvregate 22", "98434344", "09079089332")
+    f2 = Foresatt(2, "Siv Hansen", "Øvregate 22", "98434312", "09079233221")
+"""

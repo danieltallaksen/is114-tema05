ea93a0aab372de0821e66b773162b52d0af06610
Daniel Tallaksen <danieltallaksen@gmail.com>
2024-12-06 13:35:07 +0100

    endra på ting

--- barnehage/kg.py
+++ barnehage/kg.py
@@ -1,14 +1,10 @@
-from flask import Flask
-from flask import url_for
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import session
-from kgmodel import (Foresatt, Barn, Soknad, Barnehage)
+from flask import Flask, render_template, request, redirect, session, url_for
+from kgmodel import Foresatt, Barn, Soknad, Barnehage
 from kgcontroller import (form_to_object_soknad, insert_soknad, commit_all, select_alle_barnehager)
+import pandas as pd
 
 app = Flask(__name__)
-app.secret_key = 'BAD_SECRET_KEY' # nødvendig for session
+app.secret_key = 'BAD_SECRET_KEY'  # Nødvendig for session
 
 @app.route('/')
 def index():
@@ -27,29 +23,45 @@ def behandle():
         log = insert_soknad(form_to_object_soknad(sd))
         print(log)
         session['information'] = sd
-        return redirect(url_for('svar')) #[1]
+        commit_all()  # Sørg for at vi skriver til Excel etter innsending av søknad
+        return redirect(url_for('svar'))
     else:
         return render_template('soknad.html')
 
 @app.route('/svar')
 def svar():
     information = session['information']
-    return render_template('svar.html', data=information)
+    pri = information['liste_over_barnehager_prioritert_5']
+    bh_liste = select_alle_barnehager()
+    available_spots = []
+    for el in pri.split(','):
+        for bh_el in bh_liste:
+            if bh_el.barnehage_id == int(el):
+                if bh_el.barnehage_ledige_plasser > 0:
+                    available_spots.append(bh_el)
+    pri_rights = False
+    return render_template('svar.html',
+                           data=information,
+                           available_spots=available_spots,
+                           priority_rights=pri_rights)
 
 @app.route('/commit')
 def commit():
-    commit_all()
-    return render_template('commit.html')
+    # Etter commit_all(), last inn de nyeste dataene fra Excel
+    global forelder, barnehage, barn, soknad
+    forelder = pd.read_excel('kgdata.xlsx', sheet_name='foresatt')
+    barnehage = pd.read_excel('kgdata.xlsx', sheet_name='barnehage')
+    barn = pd.read_excel('kgdata.xlsx', sheet_name='barn')
+    soknad = pd.read_excel('kgdata.xlsx', sheet_name='soknad')
 
+    # Konverter DataFrames til dictionaries for visning i HTML
+    data_forelder = forelder.to_dict(orient='records')
+    data_barnehage = barnehage.to_dict(orient='records')
+    data_barn = barn.to_dict(orient='records')
+    data_soknad = soknad.to_dict(orient='records')
 
-
-
-"""
-Referanser
-[1] https://stackoverflow.com/questions/21668481/difference-between-render-template-and-redirect
-"""
-
-"""
-Søkeuttrykk
-
-"""
+    return render_template('commit.html',
+                           data_forelder=data_forelder,
+                           data_barnehage=data_barnehage,
+                           data_barn=data_barn,
+                           data_soknad=data_soknad)
--- barnehage/kgcontroller.py
+++ barnehage/kgcontroller.py
@@ -1,205 +1,127 @@
-# kgcontroller module
 import pandas as pd
-import numpy as np
-from dbexcel import *
-from kgmodel import *
+from dbexcel import forelder, barnehage, barn, soknad
+from kgmodel import Foresatt, Barn, Soknad, Barnehage
 
+# CRUD-metoder
 
-# CRUD metoder
-
-# Create
-# pd.append, pd.concat eller df.loc[-1] = [1,2] df.index = df.index + 1 df = df.sort_index()
 def insert_foresatt(f):
-    # Ikke en god praksis å oppdaterer DataFrame ved enhver endring!
-    # DataFrame er ikke egnet som en databasesystem for webapplikasjoner.
-    # Vanligvis bruker man databaseapplikasjoner som MySql, Postgresql, sqlite3 e.l.
-    # 3 fremgangsmåter for å oppdatere DataFrame:
-    # (1) df.colums er [['a', 'b']]
-    #     df = pd.concat([pd.DataFrame([[1,2]], columns=df.columns), df], ignore_index=True)
-    # (2) df = df.append({'a': 1, 'b': 2}, ignore_index=True)
-    # (3) df.loc[-1] = [1,2]
-    #     df.index = df.index + 1
-    #     df = df.sort_index()
+    """Legger til en ny foresatt i DataFrame."""
     global forelder
-    new_id = 0
-    if forelder.empty:
-        new_id = 1
-    else:
-        new_id = forelder['foresatt_id'].max() + 1
+    # Sjekk om DataFrame er tom og initialiser hvis nødvendig
+    if 'foresatt_id' not in forelder.columns:
+        forelder['foresatt_id'] = pd.Series(dtype='int')  # Initialiser foresatt_id kolonne hvis ikke finnes
     
-    # skriv kode for å unngå duplikater
+    # Sørg for at foresatt_id er numerisk (tving konvertering til int)
+    forelder['foresatt_id'] = pd.to_numeric(forelder['foresatt_id'], errors='coerce')
     
+    # Sett foresatt_id til neste ledige ID
+    new_id = 1  # Start med ID 1 hvis DataFrame er tom
+    if not forelder.empty:
+        new_id = forelder['foresatt_id'].max(skipna=True) + 1  # Hent neste ledige ID
+    
+    # Legg til ny foresatt i DataFrame
     forelder = pd.concat([pd.DataFrame([[new_id,
-                                        f.foresatt_navn,
-                                        f.foresatt_adresse,
-                                        f.foresatt_tlfnr,
-                                        f.foresatt_pnr]],
-                columns=forelder.columns), forelder], ignore_index=True)
-    
+                                         f.foresatt_navn,
+                                         f.foresatt_adresse,
+                                         f.foresatt_tlfnr,
+                                         f.foresatt_pnr]],
+                                        columns=forelder.columns), forelder], ignore_index=True)
     
     return forelder
 
 def insert_barn(b):
+    """Legger til et nytt barn i DataFrame."""
     global barn
-    new_id = 0
-    if barn.empty:
-        new_id = 1
-    else:
-        new_id = barn['barn_id'].max() + 1
+    # Sjekk om DataFrame er tom og initialiser hvis nødvendig
+    if 'barn_id' not in barn.columns:
+        barn['barn_id'] = pd.Series(dtype='int')  # Initialiser barn_id kolonne hvis ikke finnes
     
-    # burde også sjekke for samme foresatt_pnr for å unngå duplikater
+    # Sørg for at barn_id er numerisk
+    barn['barn_id'] = pd.to_numeric(barn['barn_id'], errors='coerce')
+
+    new_id = 1  # Start med ID 1 hvis DataFrame er tom
+    if not barn.empty:
+        new_id = barn['barn_id'].max(skipna=True) + 1  # Hent neste ledige ID
     
-    barn = pd.concat([pd.DataFrame([[new_id,
-                                    b.barn_pnr]],
-                columns=barn.columns), barn], ignore_index=True)
+    # Legg til nytt barn i DataFrame
+    barn = pd.concat([pd.DataFrame([[new_id, b.barn_pnr]],
+                                   columns=barn.columns), barn], ignore_index=True)
     
     return barn
 
 def insert_soknad(s):
-    """[sok_id, foresatt_1, foresatt_2, barn_1, fr_barnevern, fr_sykd_familie,
-    fr_sykd_barn, fr_annet, barnehager_prioritert, sosken__i_barnehagen,
-    tidspunkt_oppstart, brutto_inntekt]
-    """
+    """Legger inn en ny søknad i soknad DataFrame."""
     global soknad
-    new_id = 0
-    if soknad.empty:
-        new_id = 1
-    else:
-        new_id = soknad['sok_id'].max() + 1
-    
-    
-    # burde også sjekke for duplikater
-    
+    new_id = 1  # Start med ID 1 hvis DataFrame er tom
+    if not soknad.empty:
+        new_id = soknad['sok_id'].max(skipna=True) + 1  # Hent neste ledige ID
+
     soknad = pd.concat([pd.DataFrame([[new_id,
-                                     s.foresatt_1.foresatt_id,
-                                     s.foresatt_2.foresatt_id,
-                                     s.barn_1.barn_id,
-                                     s.fr_barnevern,
-                                     s.fr_sykd_familie,
-                                     s.fr_sykd_barn,
-                                     s.fr_annet,
-                                     s.barnehager_prioritert,
-                                     s.sosken__i_barnehagen,
-                                     s.tidspunkt_oppstart,
-                                     s.brutto_inntekt]],
-                columns=soknad.columns), soknad], ignore_index=True)
+                                       s.foresatt_1.foresatt_id,
+                                       s.foresatt_2.foresatt_id,
+                                       s.barn_1.barn_id,
+                                       s.fr_barnevern,
+                                       s.fr_sykd_familie,
+                                       s.fr_sykd_barn,
+                                       s.fr_annet,
+                                       s.barnehager_prioritert,
+                                       s.sosken__i_barnehagen,
+                                       s.tidspunkt_oppstart,
+                                       s.brutto_inntekt]],
+                                      columns=soknad.columns), soknad], ignore_index=True)
     
+    # Skriv til Excel etter at søknaden er lagt til
+    commit_all()
+
     return soknad
 
-# ---------------------------
-# Read (select)
-
-def select_alle_barnehager():
-    """Returnerer en liste med alle barnehager definert i databasen dbexcel."""
-    return barnehage.apply(lambda r: Barnehage(r['barnehage_id'],
-                             r['barnehage_navn'],
-                             r['barnehage_antall_plasser'],
-                             r['barnehage_ledige_plasser']),
-         axis=1).to_list()
-
-def select_foresatt(f_navn):
-    """OBS! Ignorerer duplikater"""
-    series = forelder[forelder['foresatt_navn'] == f_navn]['foresatt_id']
-    if series.empty:
-        return np.nan
-    else:
-        return series.iloc[0] # returnerer kun det første elementet i series
-
-def select_barn(b_pnr):
-    """OBS! Ignorerer duplikater"""
-    series = barn[barn['barn_pnr'] == b_pnr]['barn_id']
-    if series.empty:
-        return np.nan
-    else:
-        return series.iloc[0] # returnerer kun det første elementet i series
-    
-    
-# --- Skriv kode for select_soknad her
-
-
-# ------------------
-# Update
-
-
-# ------------------
-# Delete
-
-
-# ----- Persistent lagring ------
 def commit_all():
-    """Skriver alle dataframes til excel"""
-    with pd.ExcelWriter('kgdata.xlsx', mode='a', if_sheet_exists='replace') as writer:  
-        forelder.to_excel(writer, sheet_name='foresatt')
-        barnehage.to_excel(writer, sheet_name='barnehage')
-        barn.to_excel(writer, sheet_name='barn')
-        soknad.to_excel(writer, sheet_name='soknad')
-        
-# --- Diverse hjelpefunksjoner ---
+    """Skriver alle dataframes til Excel."""
+    with pd.ExcelWriter('kgdata.xlsx', mode='a', if_sheet_exists='replace') as writer:
+        forelder.to_excel(writer, sheet_name='foresatt', index=False)
+        barnehage.to_excel(writer, sheet_name='barnehage', index=False)
+        barn.to_excel(writer, sheet_name='barn', index=False)
+        soknad.to_excel(writer, sheet_name='soknad', index=False)
+    print("Alle data er skrevet til kgdata.xlsx")
+
 def form_to_object_soknad(sd):
-    """sd - formdata for soknad, type: ImmutableMultiDict fra werkzeug.datastructures
-Eksempel:
-ImmutableMultiDict([('navn_forelder_1', 'asdf'),
-('navn_forelder_2', ''),
-('adresse_forelder_1', 'adf'),
-('adresse_forelder_2', 'adf'),
-('tlf_nr_forelder_1', 'asdfsaf'),
-('tlf_nr_forelder_2', ''),
-('personnummer_forelder_1', ''),
-('personnummer_forelder_2', ''),
-('personnummer_barnet_1', '234341334'),
-('personnummer_barnet_2', ''),
-('fortrinnsrett_barnevern', 'on'),
-('fortrinnsrett_sykdom_i_familien', 'on'),
-('fortrinnsrett_sykdome_paa_barnet', 'on'),
-('fortrinssrett_annet', ''),
-('liste_over_barnehager_prioritert_5', ''),
-('tidspunkt_for_oppstart', ''),
-('brutto_inntekt_husholdning', '')])
-    """
-    # Lagring i hurtigminne av informasjon om foreldrene (OBS! takler ikke flere foresatte)
+    """Konverterer formdata til Soknad-objekt."""
     foresatt_1 = Foresatt(0,
                           sd.get('navn_forelder_1'),
                           sd.get('adresse_forelder_1'),
                           sd.get('tlf_nr_forelder_1'),
                           sd.get('personnummer_forelder_1'))
-    insert_foresatt(foresatt_1)
     foresatt_2 = Foresatt(0,
                           sd.get('navn_forelder_2'),
                           sd.get('adresse_forelder_2'),
                           sd.get('tlf_nr_forelder_2'),
                           sd.get('personnummer_forelder_2'))
-    insert_foresatt(foresatt_2) 
     
-    # Dette er ikke elegang; kunne returnert den nye id-en fra insert_ metodene?
-    foresatt_1.foresatt_id = select_foresatt(sd.get('navn_forelder_1'))
-    foresatt_2.foresatt_id = select_foresatt(sd.get('navn_forelder_2'))
-    
-    # Lagring i hurtigminne av informasjon om barn (OBS! kun ett barn blir lagret)
-    barn_1 = Barn(0, sd.get('personnummer_barnet_1'))
-    insert_barn(barn_1)
-    barn_1.barn_id = select_barn(sd.get('personnummer_barnet_1'))
-    
-    # Lagring i hurtigminne av all informasjon for en søknad (OBS! ingen feilsjekk / alternativer)
-        
-    sok_1 = Soknad(0,
-                   foresatt_1,
-                   foresatt_2,
-                   barn_1,
-                   sd.get('fortrinnsrett_barnevern'),
-                   sd.get('fortrinnsrett_sykdom_i_familien'),
-                   sd.get('fortrinnsrett_sykdome_paa_barnet'),
-                   sd.get('fortrinssrett_annet'),
-                   sd.get('liste_over_barnehager_prioritert_5'),
-                   sd.get('har_sosken_som_gaar_i_barnehagen'),
-                   sd.get('tidspunkt_for_oppstart'),
-                   sd.get('brutto_inntekt_husholdning'))
-    
-    return sok_1
+    foresatt_1.foresatt_id = insert_foresatt(foresatt_1)['foresatt_id'].max()
+    foresatt_2.foresatt_id = insert_foresatt(foresatt_2)['foresatt_id'].max()
 
-# Testing
-def test_df_to_object_list():
-    assert barnehage.apply(lambda r: Barnehage(r['barnehage_id'],
-                             r['barnehage_navn'],
-                             r['barnehage_antall_plasser'],
-                             r['barnehage_ledige_plasser']),
-         axis=1).to_list()[0].barnehage_navn == "Sunshine Preschool"
+    # Lagring i hurtigminne av informasjon om barn
+    barn_1 = Barn(0, sd.get('personnummer_barnet_1'))
+    barn_1.barn_id = insert_barn(barn_1)['barn_id'].max()
+
+    soknad = Soknad(0,
+                    foresatt_1,
+                    foresatt_2,
+                    barn_1,
+                    sd.get('fortrinnsrett_barnevern'),
+                    sd.get('fortrinnsrett_sykdom_i_familien'),
+                    sd.get('fortrinnsrett_sykdome_paa_barnet'),
+                    sd.get('fortrinssrett_annet'),
+                    sd.get('liste_over_barnehager_prioritert_5'),
+                    sd.get('har_sosken_som_gaar_i_barnehagen'),
+                    sd.get('tidspunkt_for_oppstart'),
+                    sd.get('brutto_inntekt_husholdning'))
+    return soknad
+
+def select_alle_barnehager():
+    """Henter alle barnehager fra barnehage DataFrame og konverterer til Barnehage-objekter."""
+    return barnehage.apply(lambda r: Barnehage(r['barnehage_id'],
+                                               r['barnehage_navn'],
+                                               r['barnehage_antall_plasser'],
+                                               r['barnehage_ledige_plasser']),
+                           axis=1).to_list()
